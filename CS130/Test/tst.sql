7f476cd38853d6932f517e625fe50273724eb05c
Man Yiu Lam <manyiulam97@gmail.com>
2020-08-09 18:09:00 +0100

    CS130 Test

---
+++ CS130/Test/tst.sql
@@ -0,0 +1,4 @@
+﻿SELECT * FROM saturn WHERE char_length(custname) >= 10 and char_length(custname) <= 14
+SELECT * FROM saturn WHERE items < 12 and not (items % 2 = 0)
+SELECT * FROM saturn WHERE custname not like 'Peter %'
+SELECT * FROM saturn WHERE custname not like 'Peter %'
