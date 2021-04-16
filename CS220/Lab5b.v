392248aab146700495dad5f8ac6316320e6f021f
Manu <manyiulam97@gmail.com>
2021-04-16 20:55:54 +0100

    CS220 Labs

---
+++ CS220/Lab5b.v
@@ -0,0 +1,27 @@
+//: version "2.0-b10"
+//: property encoding = "iso8859-1"
+//: property locale = "en"
+//: property prefix = "_GG"
+//: property title = "Lab5b.v"
+//: property timingViolationMode = 2
+//: property initTime = "0 ns"
+
+`timescale 1ns/1ns
+
+//: /netlistBegin main
+module main;    //: root_module
+//: enddecls
+
+
+endmodule
+//: /netlistEnd
+
+//: /netlistBegin JKFlipFlop
+module JKFlipFlop();
+//: interface  /sz:(183, 138) /bd:[ Li0>J(28/138) Li1>CLK(58/138) Li2>K(88/138) Bi0>RESET(94/183) Ro0<Q(40/138) Ro1<Qinv(93/138) ] /pd: 0 /pi: 0 /pe: 1 /pp: 1
+//: enddecls
+
+
+endmodule
+//: /netlistEnd
+
